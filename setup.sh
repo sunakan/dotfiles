cb43c1b25aa43bef0a719a326c9c6c92ab5136c4
sunakan <sunakan03@gmail.com>
2019-08-10 10:45:24 +0900

    [Add] よく使うパッケージを導入(apt系を使ってる)

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 #=======================================================
+# パッケージ
+#=======================================================
+sudo sed -i.bak -e "s/http:\/\/archive\.ubuntu\.com/http:\/\/jp\.archive\.ubuntu\.com/g" /etc/apt/sources.list
+sudo apt update \
+  && sudo apt install -y vim tree make ansible git tig wget
+#=======================================================
 # sshrc
 # sshrc_commandとしているのはファイル名が被っているから
 #=======================================================
